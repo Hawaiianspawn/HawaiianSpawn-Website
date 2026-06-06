543033e2277f926b271eb28c501b57468e94edce
Aaron Low <Hawaiian_spawn@live.com>
2026-06-06 12:23:56 -0700

    Configure GitHub Pages custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // Replace with your custom domain once set up, e.g. 'https://aaronlow.dev'
-  // If using GitHub Pages without a custom domain: 'https://Hawaiianspawn.github.io'
-  site: 'https://Hawaiianspawn.github.io',
+  site: 'https://hawaiianspawn.com',
 });
